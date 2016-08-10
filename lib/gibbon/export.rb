706f0262f09822152feb1f2793d804997fbc4caf
Mauricio Gomes <mauricio@edge14.com>
2016-08-10 14:11:22 -0400

    Don’t JSON encode the params

--- lib/gibbon/export.rb
+++ lib/gibbon/export.rb
@@ -19,7 +19,7 @@ module Gibbon
 
       api_url = export_api_url + method + "/"
       params = @default_params.merge(params).merge({:apikey => @api_key})
-      response = self.class.post(api_url, :body => MultiJson.dump(params), :timeout => @timeout)
+      response = self.class.post(api_url, :body => params, :timeout => @timeout)
 
       lines = response.body.lines
       if @throws_exceptions
